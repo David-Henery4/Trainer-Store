43dc7de638df56981ef46494696f399916a649a5
David Henery <david4henery00@hotmail.com>
2026-09-09 14:03:05 +0100

    Frontend: Updated frontend single product fetch error handling

--- frontend/src/routes/products/[id]/+page.ts
+++ frontend/src/routes/products/[id]/+page.ts
@@ -1,7 +1,23 @@
 
 export async function load ({fetch, params}){
   const response = await fetch(`http://localhost:8000/api/products/${params.id}`);
+
+
+  if (!response.ok){
+    const error = await response.json()
+    console.error(error);
+    throw new Error(error.error ?? "Something went wrong")
+  }
+
+  if (response.status === 422) {
+		const data = await response.json();
+
+		console.log(data.errors);
+    return {errors: data.errors}
+	}
+
   const product = await response.json()
+
   return {
     product
   }
